c056afa66644d8d25f33d60742955bc9c93b84c6
elisa_mrch <elisamarchand@mail>
2023-06-02 08:04:36 +0200

    seance trois suiite

--- Seance3/arbres_insert.c
+++ Seance3/arbres_insert.c
@@ -17,10 +17,22 @@
  *   - l'adresse du point contenant v si v existe dans l'arborescence
  *   - NULL, sinon
  */
-//  rechercher_v()
-// {
-// // TO DO
-// }
+cell_lvlh_t * rechercher_v(cell_lvlh_t * racine, char v)
+{
+    cell_lvlh_t* cour = racine;
+    pile_t* pile = initPile(taille);
+    while(cour != NULL && cour->val != v)
+    {
+        empiler(pile, cour, 0);
+        cour = cour->lv;
+        while(cour == NULL && !estVidePile(pile))
+        {
+            cour = depiler(pile, cour, 0);
+            cour = cour->lh;
+        }   
+    }
+    return cour;
+}
 
 /**
  * @brief rechercher le double prec de w dans une liste de fils
@@ -28,10 +40,15 @@
  * @param [in] w la valeur a inserer
  * @return l'adresse du pointeur prec apres lequel w doit etre inseree
  */
-//  rechercherPrecFilsTries()
-// {
-// // TO DO
-// }
+cell_lvlh_t ** rechercherPrecFilsTries(cell_lvlh_t * adrpere, char w)
+{
+    cell_lvlh_t* pprec = adrpere->lv;
+    while (pprec != NULL && pprec->val <= w)
+    {
+        pprec = pprec->lh;
+    }
+    return *pprec;
+}
 
 /** TO DO
  * @brief inserer une valeur w dans les fils d'un point de valeur v
@@ -40,7 +57,21 @@
  * @param [in] w la valeur a inserer
  * @return 1 - insertion realisee; 0 - insertion n'a pas ete realisee
  */
-//  insererTrie()
-// {
-// // TO DO
-// }
+int insererTrie(cell_lvlh_t * racine, char v, char w)
+{
+    int code_retour = 0;
+    cell_lvlh_t* adrpere = rechercher_v(racine, v);
+    if(adrpere != NULL)
+    {
+        cell_lvlh_t* pprec = rechercherPrecFilsTries(adrpere, w);
+        cell_lvlh_t* nouv = malloc(sizeof(cell_lvlh_t));
+        if(nouv != NULL)
+        {
+            nouv->val = w;
+            nouv->lv = NULL;
+            nouv->lh = pprec;
+            code_retour = 1; 
+        }
+    }
+    return code_retour;
+}
