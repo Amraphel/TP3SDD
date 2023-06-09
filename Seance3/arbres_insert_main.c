1d2f631707e3f7c1d9dd7272b76eb249497982a4
ElisaMrch <elisamarchand@orange.fr>
2023-06-09 15:03:26 +0200

    com

--- Seance3/arbres_insert_main.c
+++ Seance3/arbres_insert_main.c
@@ -33,84 +33,105 @@ TEST(nouvCell) {//TEST création d'une cellule
 
 TEST(rechercher_v) {//TEST recherche valeur 
 	int nbRacines = 0; // initialisation nombre de racine
-	int nbEltsPref = 0; // initialisation
-	eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];
-	cell_lvlh_t *racine = NULL;
-	cell_lvlh_t *pere = NULL;
+	int nbEltsPref = 0; //initialisation nombre totale d'élément dans le fichier
+	eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];//initialisation du tableau des éléments
+	cell_lvlh_t *racine = NULL;//initialisation du pointeur de la racine de l'arbre
+	cell_lvlh_t *pere = NULL; //initialisation pointeur du pere
 
 	printf("\033[35m\nrechercher_v :");
 	printf("\033[0m\n");
 
-	nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);
-	racine = pref2lvlh(tabEltPref, nbRacines);
+	nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref); //récuperation du nombre de racine
+	racine = pref2lvlh(tabEltPref, nbRacines);//création de l'arbre et récupération de l'adresse de la première racine
 
-	pere = rechercher_v(racine, 'X');   // valeur inexistante
-	CHECK( NULL == pere );
+	pere = rechercher_v(racine, 'X'); //chercher un noeud de valeur X
+	CHECK( NULL == pere );// pere de valeur X n'existe pas
 
-	pere = rechercher_v(racine, 'A');   // valeur a la racine
+	pere = rechercher_v(racine, 'A'); //chercher un noeud de valeur A
 	REQUIRE( NULL != pere );
-	CHECK( 'A' == pere->val );
+	CHECK( 'A' == pere->val ); // le pere est de valeur A
 
-	pere = rechercher_v(racine, 'Z');   // valeur qui n'existe pas
-	CHECK( NULL == pere );
+	pere = rechercher_v(racine, 'Z'); //chercher un noeud de valeur Z
+	CHECK( NULL == pere ); // valeur qui n'existe pas
 
-	pere = rechercher_v(racine, 'J');   // valeur du dernier fils de B
+	pere = rechercher_v(racine, 'J'); //chercher un noeud de valeur J
 	REQUIRE( NULL != pere );
-	CHECK( 'J' == pere->val );
+	CHECK( 'J' == pere->val );//le pere est de valeur J
 
-	libererArbre(&racine);
+	libererArbre(&racine); //liberartion de l'espace memoire de l'arbre
+
+	//LEXIQUE variables locales:
+	//nbRacines -> nombre de racine au début du fichier
+	//nbEltsPref -> nombre d'élement totale du fichier (taille max du tableau de donnée)
+	//tabEltPref -> tableau des éléments contenus dans le fichier
+	//pere -> l'adresse du noeud de valeur v
 }
 
 TEST(rechercherPrecFilsTries) {
-	int nbRacines = 0;
-	int nbEltsPref = 0;
-	eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];
-	cell_lvlh_t *racine = NULL;
-	cell_lvlh_t *pere = NULL;
-	cell_lvlh_t **pprec = NULL;
+	int nbRacines = 0; // initialisation nombre de racine
+	int nbEltsPref = 0; //initialisation nombre totale d'élément dans le fichier
+	eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX]; //initialisation tableau des éléments
+	cell_lvlh_t *racine = NULL; //initialisation du pointeur de l'arbre
+	cell_lvlh_t *pere = NULL; //initialisation pointeur du pere
+	cell_lvlh_t **pprec = NULL; //initialisatin pointeur precedant 
 
 	printf("\033[34m\nrechercherPrecFilsTries :");
 	printf("\033[0m\n");
-	nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);
-	racine = pref2lvlh(tabEltPref, nbRacines);
+	nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);//récuperation du nombre de racine
+	racine = pref2lvlh(tabEltPref, nbRacines); //création de l'arbre et récupération de l'adresse de la première racine
 
-	pere = rechercher_v(racine, 'F');
+	//Recherche F dans l'arbre
+	pere = rechercher_v(racine, 'F'); 
 	REQUIRE( NULL != pere );
 	CHECK( 'F' == pere->val );
 
+	//Recherche ou inserer L dans les fils de F
 	pprec = rechercherPrecFilsTries(pere, 'L');
 	REQUIRE( NULL != *pprec );
-	CHECK( 'M' == (*pprec)->val );
+	CHECK( 'M' == (*pprec)->val ); //placer au milieu avant M
 
+	//Recherche ou inserer A dans les fils de F
 	pprec = rechercherPrecFilsTries(pere, 'A');
 	REQUIRE( NULL != *pprec );
-	CHECK( 'K' == (*pprec)->val );
+	CHECK( 'K' == (*pprec)->val ); //placer au debut avant K
 
+	//Recherche ou inserer Z dans les fils de F
 	pprec = rechercherPrecFilsTries(pere, 'Z');
-	CHECK( NULL == (*pprec));
+	CHECK( NULL == (*pprec)); //placer a la fin
 
+	//Recherche C dans l'arbre
 	pere = rechercher_v(racine, 'C');
+	//Recherche ou inserer F dans les fils de C
 	pprec = rechercherPrecFilsTries(pere, 'F');
 	REQUIRE( NULL != *pprec );
-	CHECK( 'I' == (*pprec)->val );
+	CHECK( 'I' == (*pprec)->val ); //placer avant I
 
-	libererArbre(&racine);
+	libererArbre(&racine);//liberartion de l'espace memoire de l'arbre
+
+	//LEXIQUE variables locales:
+	//nbRacines -> nombre de racine au début du fichier
+	//nbEltsPref -> nombre d'élement totale du fichier (taille max du tableau de donnée)
+	//tabEltPref -> tableau des éléments contenus dans le fichier
+	//racine -> adresse premier element de l'arbre
+	//pere -> l'adresse du noeud de valeur v
+	//pprec -> pointeur précédant pointe sur la cellule avant laquelle la cellule sera inserée
 }
 
 TEST(insererTrie) {
-	int nbRacines = 0;
-	int nbEltsPref = 0;
-	eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];
-	cell_lvlh_t *racine = NULL;
+	int nbRacines = 0; // initialisation nombre de racine
+	int nbEltsPref = 0; //initialisation nombre totale d'élément dans le fichier
+	eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];//initialisation tableau des éléments
+	cell_lvlh_t *racine = NULL; //initialisation du pointeur de l'arbre
 
 	printf("\033[34m\ninsererTrie :");
 	printf("\033[0m\n");
-	nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);
-	racine = pref2lvlh(tabEltPref, nbRacines);
+	nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);//récuperation du nombre de racine
+	racine = pref2lvlh(tabEltPref, nbRacines); //création de l'arbre et récupération de l'adresse de la première racine
 
 	printf("\033[34mPremiere Insertion : B dans A");
 	printf("\033[0m\n");
 	insererTrie(racine,'A','B');
+	//VERIFICATIONS: B devient le fils de A
 	CHECK('B' == racine->lv->val);
 	CHECK('B' == racine->lv->lh->val);
 	CHECK('D' == racine->lv->lh->lh->val);
@@ -118,6 +139,7 @@ TEST(insererTrie) {
 	printf("\033[34mDeuxieme Insertion : A dans A");
 	printf("\033[0m\n");
 	insererTrie(racine, 'A', 'A');
+	//VERIFICATIONS: A devient le fils de A
 	CHECK('A' == racine->lv->val);
 	CHECK('B' == racine->lv->lh->val);
 	CHECK('B' == racine->lv->lh->lh->val);
@@ -126,6 +148,7 @@ TEST(insererTrie) {
 	printf("\033[34mTroisieme Insertion : L dans F");
 	printf("\033[0m\n");
 	insererTrie(racine, 'F', 'L');
+	//VERIFICATIONS: L devient le fils de F
 	CHECK('F' == racine->lh->lv->val);
 	CHECK('K' == racine->lh->lv->lv->val);
 	CHECK('L' == racine->lh->lv->lv->lh->val);
@@ -135,6 +158,7 @@ TEST(insererTrie) {
 	printf("\033[34mQuatrieme Insertion : Z dans F");
 	printf("\033[0m\n");
 	insererTrie(racine, 'F', 'Z');
+	//VERIFICATIONS: Z devient le fils de F
 	CHECK('F' == racine->lh->lv->val);
 	CHECK('K' == racine->lh->lv->lv->val);
 	CHECK('L' == racine->lh->lv->lv->lh->val);
@@ -143,7 +167,13 @@ TEST(insererTrie) {
 	CHECK('Z' == racine->lh->lv->lv->lh->lh->lh->lh->val);
 
 
-	libererArbre(&racine);
+	libererArbre(&racine);//liberartion de l'espace memoire de l'arbre
+
+	//LEXIQUE variables locales:
+	//nbRacines -> nombre de racine au début du fichier
+	//nbEltsPref -> nombre d'élement totale du fichier (taille max du tableau de donnée)
+	//tabEltPref -> tableau des éléments contenus dans le fichier
+	//racine -> adresse premier element de l'arbre
 }
 
 END_TEST_GROUP(ARBRE_INSERT)
